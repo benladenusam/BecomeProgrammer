e2da3f12ddd59b24ceb8e6c14d7c284bbb72d6cf
benladenusam <volodimi@meta.ua>
2019-01-30 21:24:40 +0200

    Fixed navbar and made a half of the first task!

--- personal/views.py
+++ personal/views.py
@@ -3,5 +3,11 @@ from django.utils import timezone
 from .models import Post
 
 def index(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'personal/home.html', {'posts': posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date').reverse()
+    return render(request, 'personal/home.html', {
+        'posts': posts,
+        'nbar': 'home',
+        })
+
+def adaptive(request):
+    return render(request, 'personal/adaptive.html', {'nbar': 'adaptive'})
